7950247a4703adbd4570b961714c5b455b80cb6d
Hakki999 <josemiguelbar9@gmail.com>
2022-06-19 17:39:13 -0300

    v1_19/06_2022_17:39

---
+++ main.js
@@ -0,0 +1,20 @@
+ScrollReveal().reveal('.hr', {
+    distance: '150%',
+    origin: 'top',
+    delay: 0,
+    reset: true
+});
+
+ScrollReveal().reveal('.hrr', {
+    distance: '150%',
+    origin: 'top',
+  delay: 200,
+    reset: true
+});
+
+ScrollReveal().reveal('.hrrr', {
+    distance: '150%',
+    origin: 'top',
+    delay: 300,
+    reset: true
+});
